56a6643d7b1f30231a18448568d06a73de29c697
MohitGarg1995 <MohitGarg1995@gmail.com>
2024-10-01 21:14:55 +0530

    redirect user to new page

--- Routes/ccAvenue.route.js
+++ Routes/ccAvenue.route.js
@@ -11,15 +11,24 @@ ccAvenueRouter.post("/", createDonationEntry);
 ccAvenueRouter.post("/response", postDonationResponse);
 
 // Success Page Endpoint
-ccAvenueRouter.get("/success", (req, res) => {
-  res.send("Transaction Successful! Thank you for your donation.");
-});
-// Failed Page Endpoint
-ccAvenueRouter.get("/failed", (req, res) => {
-  res.send("Transaction Failed! Please try again.");
+ccAvenueRouter.post("/success", (req, res) => {
+  // res.redirect("Transaction Successful! Thank you for your donation.");
+  res.json({
+    message: "Transaction Successful!",
+    redirectUrl: "https://www.example.com/success",
+  });
 });
 
-ccAvenueRouter.get("/", (req, res) => {
+// Failed Page Endpoint
+ccAvenueRouter.post("/failed", (req, res) => {
+  // res.redirect("Transaction Failed! Please try again.");
+  res.json({
+    message: "Transaction Failed!",
+    redirectUrl: "https://www.example.com/success",
+  });
+});
+
+ccAvenueRouter.post("/", (req, res) => {
   res.send("CC Avenue Response Base Endpoint");
 });
 
