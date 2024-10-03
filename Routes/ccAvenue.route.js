7e9c802f89a4f454decc58cbda4074651d9debbc
Webcodeft Technologies <webcodeft@gmail.com>
2024-10-03 08:25:53 +0530

    Update ccAvenue.route.js
    createDonationEntry

--- Routes/ccAvenue.route.js
+++ Routes/ccAvenue.route.js
@@ -7,7 +7,7 @@ import {
 const ccAvenueRouter = express.Router();
 
 // Route to handle donation entry creation
-ccAvenueRouter.post("/", postDonationResponse);
+ccAvenueRouter.post("/", createDonationEntry);
 ccAvenueRouter.post("/response", postDonationResponse);
 
 // Success Page Endpoint
