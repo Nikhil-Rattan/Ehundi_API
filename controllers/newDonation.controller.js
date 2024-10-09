7d815a1f3fabcb0799248615b1a4335c9413cb5b
Mohit Garg <tl.webcodeft@gmail.com>
2024-10-09 10:55:25 +0530

    removes the unwanted comment

--- controllers/newDonation.controller.js
+++ controllers/newDonation.controller.js
@@ -48,47 +48,6 @@ export const createDonation = async (req, res) => {
   }
 };
 
-// export const getDonationById = async (req, res) => {
-//   try {
-//     console.log("Donation ID:", req.params.donationId);
-//     // Extract the donationId from the request parameters
-//     const { name } = req.params;
-//     console.log("name:-",name)
-
-//     // if (!mongoose.Types.ObjectId.isValid(name)) {
-//     //   return res.status(400).json({
-//     //     success: false,
-//     //     // message: "Invalid user ID",
-//     //   });
-//     // }
-
-//     // Find the donation by its ID
-//     const donation = await NewDonation.find(name);
-//     console.log("donation",donation);
-
-//     // If donation not found, send a 404 response
-//     if (!donation) {
-//       return res.status(404).json({
-//         success: false,
-//         message: "Donation not found",
-//       });
-//     }
-
-//     // If donation found, send it in the response
-//     return res.status(200).json({
-//       success: true,
-//       donation,
-//     });
-//   } catch (error) {
-//     // Handle any errors during the process
-//     return res.status(500).json({
-//       success: false,
-//       message: "Error retrieving donation",
-//       error: error.message,
-//     });
-//   }
-// };
-
 export const getDonationById = async (req, res) => {
   try {
     // Extract the name from the request parameters
@@ -130,9 +89,9 @@ export const getDonationById = async (req, res) => {
 
 export const getAllDonations = async (req, res) => {
   try {
+     console.log('Fetching donations...');
     const { user } = req.query;
     const now = new Date();    
-
     const lastMonth = new Date(now.setMonth(now.getMonth() - 1));
 
     let donations = {};
@@ -150,51 +109,13 @@ export const getAllDonations = async (req, res) => {
       });
     }
 
-    // 1. Total number of donations
-    // const totalDonations = await NewDonation.countDocuments();
-
-    // 2. Total donations in the last month
-    // const totalDonationsLastMonth = await NewDonation.countDocuments({
-    //   createdAt: { $gte: lastMonth }
-    // });
-
-     // 3. Total donated amount
-    //  const totalDonatedAmount = await NewDonation.aggregate([
-    //   {
-    //     $group: {
-    //       _id: null,
-    //       totalAmount: { $sum: "$donationAmount" }
-    //     }
-    //   }
-    // ]);
-
-    // 4. Total donated amount in the last month
-    // const totalDonatedAmountLastMonth = await NewDonation.aggregate([
-    //   {
-    //     $match: {
-    //       createdAt: { $gte: lastMonth }
-    //     }
-    //   },
-    //   {
-    //     $group: {
-    //       _id: null,
-    //       totalAmount: { $sum: "$donationAmount" }
-    //     }
-    //   }
-    // ]);
-    // Return the list of donations
     return res.status(200).json({
       success: true,
-      // data: {
-      //   totalDonations,
-      //   totalDonationsLastMonth,
-      //   totalDonatedAmount: totalDonatedAmount[0]?.totalAmount || 0,
-      //   totalDonatedAmountLastMonth: totalDonatedAmountLastMonth[0]?.totalAmount || 0
-      // },
       donations,
     });
   } catch (error) {
     // Handle any errors during the process
+    console.error("Error retrieving donations:", error.message);
     return res.status(500).json({
       success: false,
       message: "Error retrieving donations",
